0e6e3f15a8c19bbb8338d22e5bad20d07e22c98f
wangjianfei <hzwangjianfei@corp.netease.com>
2016-01-05 16:47:39 +0800

    "1"

---
+++ podtest/podtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  podtest
+//
+//  Created by wangjianfei on 16/1/5.
+//  Copyright © 2016年 wangjianfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ podtest/podtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  podtest
+//
+//  Created by wangjianfei on 16/1/5.
+//  Copyright © 2016年 wangjianfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
